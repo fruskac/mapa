9c0ca2f4036a10c6caac140568801bf5e37a82d4
Mladen Plavsic <mladen.plavsic@gmail.com>
2017-02-13 22:41:07 +0100

    moved dependencies outside src

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Mapa | Fruška gora, Srbija
+
+## Contributing
+
+##### Branching model
+We are using `git-flow` as our brancing model. Read more at http://nvie.com/posts/a-successful-git-branching-model/
+
+##### Tools
+To be able to build source files, you need to have `npm` and `node` installed. After that run
+```
+npm install
+```
+This will install `Gulp` with all needed parts.
+
+##### Dependencies
+Dependencies are handled via `bower`. To get dependecies run
+```
+bower install
+```
+
+##### Build
+To create build, run
+```
+gulp
+```
+
--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,32 @@ var cssnano = require('gulp-cssnano');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 
+gulp.task('default', [
+  'vendor'
+]);
+
+gulp.task('vendor', [
+  'vendor_js',
+  'vendor_css'
+]);
+
+gulp.task('vendor_js', function () {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/BigScreen/bigscreen.min.js',
+    'bower_components/simplebar/dist/simplebar.min.js',
+    'bower_components/gmaps-marker-clusterer/src/markerclusterer.js'
+  ])
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('vendor_css', function () {
+  return gulp.src([
+    'bower_components/simplebar/dist/simplebar.css'
+  ])
+    .pipe(concat('vendor.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist'))
+});
